5794a95edc33614ddf85dbe5a374ac51f5b99974
anechaBoon <anecha.boon@gmail.com>
2025-11-02 12:48:08 +0700

    Update clearUploads

--- routes/images.js
+++ routes/images.js
@@ -147,6 +147,12 @@ router.delete('/clearUploads', async (req, res) => {
         const filePath = path.join(uploadDir, file);
         fs.unlinkSync(filePath);
       });
+
+      // delete all records in images table
+      await db.query("DELETE FROM images");
+      // delete all records in image_has_hashtags table
+      await db.query("DELETE FROM image_has_hashtags");
+      
       res.json({ status: true, msg: 'All uploaded files deleted' });
     } else {
       res.status(400).json({ status: false, msg: 'Uploads folder does not exist' });
