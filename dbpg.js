c95f26b55efe5ae921caaedbda6a4b8f9221f9cb
anechaBoon <anecha.boon@gmail.com>
2025-11-02 10:17:21 +0700

    Update dbpg.js

--- dbpg.js
+++ dbpg.js
@@ -8,6 +8,9 @@ export const db = new Pool({
   host: process.env.DB_HOST,   
   user: process.env.DB_USER,    
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,    
-  port: process.env.DB_PORT,          
+  connectionString: process.env.DB_DATABASE,    
+  port: process.env.DB_PORT,   
+  ssl: {
+    rejectUnauthorized: false, // ✅ จำเป็นใน Render
+  },       
 });
