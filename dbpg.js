e453245e0aef4fc247a4f4ee5629121a94fe8af3
anechaBoon <anecha.boon@gmail.com>
2025-11-02 10:22:39 +0700

    Update dbpg.js

--- dbpg.js
+++ dbpg.js
@@ -7,8 +7,8 @@ const { Pool } = pkg;
 export const db = new Pool({
   host: process.env.DB_HOST,   
   user: process.env.DB_USER,    
-  password: process.env.DB_PASSWORD,
-  connectionString: process.env.DB_DATABASE,    
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,   
   ssl: {
     rejectUnauthorized: false, // ✅ จำเป็นใน Render
